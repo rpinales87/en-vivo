56eaedb03ba3012799e078bb2f01e7891980a4be
rpinales87 <pinales.roberto@gmail.com>
2019-06-03 14:53:12 -0400

    Created curl scripts to get events for a city

--- curl-scripts/get-events.sh
+++ curl-scripts/get-events.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ID="24580"
+KEY="sCB2nJUZVMYLC2zN"
 
-curl "https://api.songkick.com/api/3.0/metro_areas/24580/calendar.json?apikey=sCB2nJUZVMYLC2zN" \
+curl "https://api.songkick.com/api/3.0/metro_areas/${ID}/calendar.json?apikey=${KEY}" \
   --include \
   --request GET \
 
